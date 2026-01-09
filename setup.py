e16ad707c67aaafb2d90a6b2e5d7241f1b1bed07
Luca Chiarelli <luca.chiarelli@exxeta.com>
2026-01-09 14:21:08 +0100

    ... add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from setuptools import setup
+from setuptools.config import read_configuration
+
+
+PROJECT_PATH = Path(__file__).parent
+config = read_configuration(PROJECT_PATH.joinpath('setup.cfg'))
+
+extras_require = {
+    'setup': config['options']['setup_requires'],
+    'test': config['options']['tests_require'],
+    **config['options']['extras_require'],
+}
+
+extras_require['all'] = [*extras_require.values()]
+kwargs = {'extras_require': extras_require}
+
+if (PROJECT_PATH / '.git').exists():
+    use_scm_version = {'write_to': Path(config['metadata']['name'].replace('-', '_'), 'version.py')}
+    kwargs['use_scm_version'] = use_scm_version
+
+setup(
+    **kwargs
+)
